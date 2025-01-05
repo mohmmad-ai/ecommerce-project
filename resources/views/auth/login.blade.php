14dc7a6113877fc8bfad1c38e9ef43b1d968bf87
mohammad-ai <101220002@mu.edu.lb>
2025-01-05 19:48:47 +0200

    modified: login,register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,17 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html>
+
+<head>
+ @include('home.css')
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+   @include('home.header')
+    <!-- end header section -->
+  </div>
+  <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -45,3 +58,8 @@
         </div>
     </form>
 </x-guest-layout>
+
+
+</body>
+
+</html>
